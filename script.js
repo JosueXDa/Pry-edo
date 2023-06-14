29bfa7676c033387fa5300faf9a827015adf069a
JosueXDa <josueloy12@gmail.com>
2023-06-14 03:21:31 -0500

    Se hizo todas las correcciones posibles para la entrega, esperemos lo mejor

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 var parameters = {
   appName: "classic",
   id: "applet",
-  width: 1200,
-  height: 800,
+  width: 1000,
+  height: 700,
+  showToolBar: false,
+  borderColor: null,
+  showMenuBar: true,
+  showAlgebraInput: true,
+  showResetIcon: true,
+  enableLabelDrags: false,
+  enableShiftDragZoom: true,
+  enableRightClick: false,
+  capturingThreshold: null,
+  showToolBarHelp: false,
+  errorDialogsActive: true,
+  useBrowserForJS: false,
+//  material_id: "s5kkvsck"
+//material_id: "qamjrsms"
+//  material_id: "g4wtthhn",
+//    material_id: "wtg6mgau"
+//huxezptf
+  material_id: "ywxqjcne"
+};
+var parameters2 = {
+  appName: "classic",
+  id: "applet",
+  width: 1000,
+  height: 700,
   showToolBar: false,
   borderColor: null,
   showMenuBar: false,
@@ -18,12 +42,19 @@ var parameters = {
 //  material_id: "s5kkvsck"
 //  material_id: "qamjrsms"
   material_id: "g4wtthhn",
+//    material_id: "wtg6mgau"
+//huxezptf
+//  material_id: "ywxqjcne"
 };
+
 
 // true to force web3d (not webSimple)
 var applet = new GGBApplet(parameters, true);
+var applet2 = new GGBApplet(parameters2, true);
+
 //  when used with Math Apps Bundle, uncomment this:
 //  applet.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
 window.onload = function () {
   applet.inject("applet_container");
+  applet2.inject("applet_container_2");
 };
